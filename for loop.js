48672f50e98d1285e8a7103537dfb74c68405652
banuhajeera <107339291+banuhajeera@users.noreply.github.com>
2022-06-21 01:56:09 +0530

    Add files via upload
    Day -3 assignment

---
+++ for loop.js
@@ -0,0 +1,9 @@
+let student  ={}
+student.firstname ="hajeera banu";
+student.age="25";
+ 
+console.log("loops in object");
+for( let key in student){
+    console.log(key);
+    console.log(student[key]);
+}
---
+++ resume.js
@@ -0,0 +1,21 @@
+let  resume ={}
+ resume.firstname="hajeera";
+ resume.lastname=" banu";
+ resume.age="26";
+ resume.qualification="B.sc electronic communication science";
+ resume.collegename="justice basheer ahment sayed college";
+ resume.ocupation="AR caller";
+ resume.currentorganisation="access healthcare";
+
+ console.log(resume)
+ 
+
+ 
+
+
+
+
+
+
+
+   
